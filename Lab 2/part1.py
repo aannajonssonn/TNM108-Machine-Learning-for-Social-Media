23d66f40adf529a46537d6cb0419159fef7bd8fa
Anna Jonsson <annjo712@student.liu.se>
2022-10-03 20:12:58 +0200

    Påbörjat Part 1 på Lab2

---
+++ Lab 2/part1.py
@@ -0,0 +1,144 @@
+# Lab 2 in the course TNM108 - Machine Learning for Social Media at Linköpings University 2022
+# Anna Jonsson and Amanda Bigelius
+
+# Dependencies
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns; sns.set()
+
+# Simple Linear Regression
+# y = kx+m
+rng = np.random.RandomState(1)
+x = 10 * rng.rand(50)
+y = 2 * x - 5 + rng.randn(50)
+plt.scatter(x, y);
+plt.show()
+
+# Linear Regression with Scikit-Learn
+from sklearn.linear_model import LinearRegression
+model = LinearRegression(fit_intercept=True)
+model.fit(x[:, np.newaxis], y)
+
+xfit = np.linspace(0, 10, 1000)
+yfit = model.predict(xfit[:, np.newaxis])
+
+plt.scatter(x, y)
+plt.plot(xfit, yfit);
+plt.show()
+
+# Print the slope and intercept of the model
+print('\n Model slope: ', model.coef_[0])
+print('\n Model intercept:', model.intercept_)
+
+# Multidimensional Linear Regression 
+# y = a0 + a1x1 + a2x2 + a3x3 + ...
+rng = np.random.RandomState(1)
+X = 10 * rng.rand(100, 3)
+y = 0.5 + np.dot(X, [1.5, -2., 1.])
+model.fit(X, y)
+print('\n Model intercept:', model.intercept_)
+print('\n Model slope: ', model.coef_)
+
+# Polynomial Regression
+# xn = fn(x) where x is our single-dimensional input.
+# y = a0 + a1f1(x) + a2f2(x) + a3f3(x) + ...
+# y = a0 + a1x + a2x^2 + a3x^3 + ...
+
+from sklearn.preprocessing import PolynomialFeatures
+x = np.array([2, 3, 4])
+poly = PolynomialFeatures(3, include_bias=False)
+poly.fit_transform(x[:, None])
+
+# Pipeline to plug in the higher-dimensional data representation into a linear regression
+from sklearn.pipeline import make_pipeline
+poly_model = make_pipeline(PolynomialFeatures(7), LinearRegression())
+
+# Use the linear model to fit the complex relationships between x and y
+rng = np.random.RandomState(1)
+x = 10 * rng.rand(50)
+y = np.sin(x) + 0.1 * rng.randn(50)
+xfit = np.linspace(0, 10, 1000)
+poly_model.fit(x[:, np.newaxis], y)
+yfit = poly_model.predict(xfit[:, np.newaxis])
+plt.scatter(x, y)
+plt.plot(xfit, yfit);
+plt.show()
+
+# Gaussian Basis Functions
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class GaussianFeatures(BaseEstimator, TransformerMixin):
+
+# """Uniformly spaced Gaussian features for one-dimensional input"""
+
+    def __init__(self, N, width_factor=2.0):
+        self.N = N
+        self.width_factor = width_factor
+
+    @staticmethod
+    def _gauss_basis(x, y, width, axis=None):
+        arg = (x - y) / width
+        return np.exp(-0.5 * np.sum(arg ** 2, axis))
+
+    def fit(self, X, y=None):
+        # create N centers spread along the data range
+        self.centers_ = np.linspace(X.min(), X.max(), self.N)
+        self.width_ = self.width_factor * (self.centers_[1] - self.centers_[0])
+        return self
+
+    def transform(self, X):
+        return self._gauss_basis(X[:, :, np.newaxis], self.centers_, self.width_, axis=1)
+
+    gauss_model = make_pipeline(GaussianFeatures(20), LinearRegression())
+    gauss_model.fit(x[:, np.newaxis], y)
+    yfit = gauss_model.predict(xfit[:, np.newaxis])
+
+    plt.scatter(x, y)
+    plt.plot(xfit, yfit)
+    plt.xlim(0, 10);
+    plt.show()
+
+# Regularization
+model = make_pipeline(GaussianFeatures(30), LinearRegression())
+model.fit(x[:, np.newaxis], y)
+plt.scatter(x, y)
+plt.plot(xfit, model.predict(xfit[:, np.newaxis]))
+plt.xlim(0, 10)
+plt.ylim(-1.5, 1.5);
+
+# Plot with the coefficients of the Gaussian bases with respect to their locations
+def basis_plot(model, title=None):
+    fig, ax = plt.subplots(2, sharex=True)
+    model.fit(x[:, np.newaxis], y)
+    ax[0].scatter(x, y)
+    ax[0].plot(xfit, model.predict(xfit[:, np.newaxis]))
+    ax[0].set(xlabel='x', ylabel='y', ylim=(-1.5, 1.5))
+
+    if title:
+        ax[0].set_title(title)
+
+    ax[1].plot(model.steps[0][1].centers_,
+               model.steps[1][1].coef_)
+    ax[1].set(xlabel='basis location',
+              ylabel='coefficient',
+              xlim=(0, 10))
+
+model = make_pipeline(GaussianFeatures(30), LinearRegression())
+basis_plot(model)
+
+# Ridge Regression (L2 regularization)
+# Penalty, P = alpha*sum(theta_n^2) 
+# where alpha is a free parameter that controls the strenght of the penalty 
+
+from sklearn.linear_model import Ridge
+model = make_pipeline(GaussianFeatures(30), Ridge(alpha=0.1))
+basis_plot(model, title='Ridge Regression')
+
+# Lasso Regression (L1 regularization)
+# Penalty, P = alpha*sum(abs(theta_n)) 
+# alpha tunes the strength of the penalty, and should be determined by cross-validation
+from sklearn.linear_model import Lasso
+model = make_pipeline(GaussianFeatures(30), Lasso(alpha=0.001))
+basis_plot(model, title='Lasso Regression')
+
+
